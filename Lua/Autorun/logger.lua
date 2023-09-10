4ec2017a0ad781e481957242dd2d66496bc26469
HomelessMage <42868046+HomelessMage@users.noreply.github.com>
2023-09-10 23:55:49 +0300

    Revert "Lua update"
    This reverts commit 422da0db845967f78f53bab03d4d89f2ad79d706.

--- Lua/Autorun/logger.lua
+++ Lua/Autorun/logger.lua
@@ -3,7 +3,7 @@ Hook.Add("client.connected", "logConnections", function (client)
     print(msg)
 end)
 
--- Срёт ошибками
--- Hook.Add('character.death', 'logDeaths', function(character)
---     print('Персонаж погиб: ', character.Name, 'Убийца: ', character.Info.CauseOfDeath.Attacker.Name)
--- end)
+
+Hook.Add('character.death', 'logDeaths', function(character)
+    print('Персонаж погиб: ', character.Name, 'Убийца: ', character.Info.CauseOfDeath.Attacker.Name)
+end)
